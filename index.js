138cc8706484547e1562d242ed5a9b68baecf4e4
iamjosephvarghese <josephvarghese.rms@gmail.com>
2019-11-14 15:29:32 +0100

    Connected to firebase using admin sdk

--- index.js
+++ index.js
@@ -5,6 +5,16 @@ const cors = require('cors')
 var admin = require('firebase-admin');
 var _ = require('lodash');
 
+
+var serviceAccount = require("./firebase_config.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://techkshetra-workshop.firebaseio.com"
+});
+
+  var db = admin.database();var ref = db.ref("questions");
+
 let app = express();
     app.use(bodyParser.urlencoded({ extended: false }));
     app.use(bodyParser.json());
@@ -16,3 +26,4 @@ app.get('/getquestions',async function(req, res){
 let server = app.listen(3000, function() {
         console.log('Server is listening on port 3000')
     });
+
